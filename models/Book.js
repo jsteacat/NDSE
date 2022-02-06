6df4f91acbc0c078e19e97c52404e2a9c297cd1c
teacat <ubscoder@gmail.com>
2022-02-06 18:34:55 +0300

    add book file

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 
 class Book {
-  constructor(title = '', description = '', authors = '', favorite = '', fileCover = '', fileName = '', id = uuidv4()) {
+  constructor(title = '', description = '', authors = '', favorite = '', fileCover = '', fileName = '', fileBook = '', id = uuidv4()) {
     this.id = id;
     this.title = title;
     this.description = description;
@@ -9,6 +9,7 @@ class Book {
     this.favorite = favorite;
     this.fileCover = fileCover;
     this.fileName = fileName;
+    this.fileBook = fileBook;
   }
 }
 
