a2ad1c35ce0c37ee7ece6c77997a56ab82e799ab
teacat <ubscoder@gmail.com>
2022-02-08 23:14:42 +0300

    precommit w/o book form && refactoring routes

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const userRouter = require('./routes/user');
+const userApiRouter = require('./routes/api/user');
+const bookApiRouter = require('./routes/api/book');
 const bookRouter = require('./routes/book');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use('/api/user', userRouter);
-app.use('/api/books', bookRouter);
+app.use('/api/user', userApiRouter);
+app.use('/api/books', bookApiRouter);
+app.use('/books', bookRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
